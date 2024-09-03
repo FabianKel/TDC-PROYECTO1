0fd4caed9b326d65ceb6e08e282676d7235cd372
FabianKel <drk.fab@gmail.com>
2024-09-02 23:36:17 -0600

    backslash fixed :D

--- infix2postfix.py
+++ infix2postfix.py
@@ -1,7 +1,7 @@
 from format_regex import convert_extensions
 
 def get_precedence(c):
-    precedences = {'(': 1, '[': 1, '{':1, '|': 2, '•': 3, '?': 4, '*': 4, '+': 4, '^': 5}
+    precedences = {'(': 1, '|': 2, '•': 3, '?': 4, '*': 4, '+': 4, '^': 5}
     return precedences.get(c, 6)  # Los operadores no listados tienen la mayor precedencia
 
 
@@ -9,23 +9,31 @@ def infix_to_postfix(regex):
     postfix = []
     stack = []
     converted_regex = convert_extensions(regex)
+    esc = False
     
     for i in range(len(converted_regex)):
         c = converted_regex[i]
+        if esc:
+            esc = False
+            continue
         if c.isalnum() or c == 'ε' or c == '.':
             postfix.append(c)
             #print(f'Output: {c} -> Postfix: {"".join(postfix)}, Stack: {stack}')
 
         elif c == '\\':
-            if converted_regex[i+1] in '([{)]}':
-                postfix.append(converted_regex[i+1])
-            if converted_regex[i+1] == 'n':
+            if converted_regex[i+1]:
                 postfix.append('\\')
+                postfix.append(converted_regex[i+1])
+                esc = True
+            continue
+        
+        elif c == 'ඞ':
+            postfix.append('\\n')
 
-        elif c in '([{':
+        elif c in '(':
             stack.append(c)
             #print(f'Push: {c} -> Postfix: {"".join(postfix)}, Stack: {stack}')
-        elif c in ')]}':
+        elif c in ')':
             opening = '(' if c == ')' else '[' if c == ']' else '{' if c == '}' else None
             while stack and stack[-1] != opening:
                 postfix.append(stack.pop())
--- tree.py
+++ tree.py
@@ -2,17 +2,26 @@ from nltk.tree import Tree
 
 def postfix_to_tree(postfix):
     stack = []
-    
-    for char in postfix:
+    for i in range(len(postfix)):
+        char = postfix[i]
+
         if char.isalnum() or char in '.@':
             stack.append(Tree(char, []))
+        elif char in '\\':
+            stack.append(Tree(postfix[i+1], []))
+            continue
+
+
         elif char in '*|•':
             if char == '*':
                 operand = stack.pop()
                 stack.append(Tree('*', [operand]))
+            
             elif char in '•|':
                 right = stack.pop()
                 left = stack.pop()
                 stack.append(Tree(char, [left, right]))
-    
+    print('------------------------------------------------')
+    print(f'ARBOL FINAL {stack}')
+    print('------------------------------------------------')
     return stack[0]
